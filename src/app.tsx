071ff51f20f630d0437fbc5c31705abb50ca7016
Stefan Pejcic <stefan@pejcic.rs>
2022-09-10 19:29:01 +0200

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -58,23 +58,6 @@ function Head() {
       />
       <meta name="twitter:image" content="/static/og/card.png" />
       <meta name="twitter:image:alt" content="The Vercel and Preact logos" />
-      <script>
-function myFunction() {
-  // Get the text field
-  var copyText = document.getElementById("myInput");
-
-  // Select the text field
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); // For mobile devices
-
-  // Copy the text inside the text field
-  navigator.clipboard.writeText(copyText.value);
-  
-  // Alert the copied text
-  alert("Copied the text: " + copyText.value);
-}
-</script>
-      
     </head>
   );
 }
