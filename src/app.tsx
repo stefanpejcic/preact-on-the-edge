44071528c38424f9991b65653931c12715e6ddb3
Stefan Pejcic <stefan@pejcic.rs>
2024-09-17 14:16:20 +0200

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,5 @@
-export default function handler(req, res) {
-  const ip = (req.headers['x-forwarded-for'] ?? req.connection.remoteAddress ?? '127.0.0.1').split(',')[0];
+export default function App({ req }) {
+  const ip = (req.headers.get('x-forwarded-for') ?? '127.0.0.1').split(',')[0];
   
-  // Set the content type to text/plain and send the IP
-  res.setHeader('Content-Type', 'text/plain');
-  res.status(200).send(ip);
+  return ip;
 }
